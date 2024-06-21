ad4889949097ca7f95f0c88615c8dffcba9c8bb2
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-06-21 18:08:42 +0800

    fix: fixed PlotPos

--- src/plotcraft/PlotPos.cc
+++ src/plotcraft/PlotPos.cc
@@ -43,7 +43,7 @@ PlotPos::PlotPos(int x, int z) : x(x), z(z) {
     auto& cfg       = config::cfg.generator;
     int   totalSize = cfg.plotChunkSize * 16; // 地皮大小，含道路宽度 (3*16=48)
     minPos          = Vec3{x * totalSize + 1, -64, z * totalSize + 1};
-    maxPos          = Vec3{minPos.x + totalSize, 320, minPos.z + totalSize};
+    maxPos          = Vec3{minPos.x + totalSize - 1, 320, minPos.z + totalSize - 1};
 #endif
 }
 
@@ -78,17 +78,19 @@ PlotPos::PlotPos(const Vec3& vec3) {
 #ifdef GEN_2
     // Generator 2
     auto& cfg       = config::cfg.generator;
-    int   plotWidth = (cfg.plotChunkSize * 16) - 2; // 地皮大小 (3*16=48)
-    int   totalSize = plotWidth + 2;                // 地皮大小 + 道路宽度
-    int   gridX     = std::floor(vec3.x / totalSize);
-    int   gridZ     = std::floor(vec3.z / totalSize);
-    int   localX    = static_cast<int>(std::floor(vec3.x)) % totalSize;
-    int   localZ    = static_cast<int>(std::floor(vec3.z)) % totalSize;
+    int   totalSize = cfg.plotChunkSize * 16; // 地皮大小 + 道路宽度 => (3*16=48)
+    int   plotWidth = totalSize - 2;          // 地皮大小 (48-2=46)
 
-    if (localX < 1) localX += totalSize;
+    int gridX = std::floor(vec3.x / totalSize);
+    int gridZ = std::floor(vec3.z / totalSize);
+    int localX = static_cast<int>(std::floor(vec3.x)) % totalSize; // 46 % 48 = 46 => (当前坐标和完整大小取模)
+    int localZ = static_cast<int>(std::floor(vec3.z)) % totalSize;
+
+    if (localX < 1) localX += totalSize; // 0 + 1 = 1 => border
     if (localZ < 1) localZ += totalSize;
 
-    if (localX >= plotWidth || localZ >= plotWidth) {
+    // 1 > 1     46 > 46
+    if (localX > plotWidth || localZ > plotWidth) {
         // Point is on the road
         minPos   = Vec3{0, 0, 0};
         maxPos   = Vec3{0, 0, 0};
@@ -99,7 +101,7 @@ PlotPos::PlotPos(const Vec3& vec3) {
         x      = gridX;
         z      = gridZ;
         minPos = Vec3{x * totalSize + 1, -64, z * totalSize + 1};
-        maxPos = Vec3{minPos.x + plotWidth, 320, minPos.z + plotWidth};
+        maxPos = Vec3{minPos.x + plotWidth - 1, 320, minPos.z + plotWidth - 1};
     }
 #endif
 }
