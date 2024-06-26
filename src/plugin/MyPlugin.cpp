26cdce6f88a664d61ffdab2f6f14b55995d1b876
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-06-26 12:05:37 +0800

    fix: 修复文件夹不存在导致的加载失败

--- src/plugin/MyPlugin.cpp
+++ src/plugin/MyPlugin.cpp
@@ -1,6 +1,7 @@
 #include "plugin/MyPlugin.h"
 
 #include <corecrt.h>
+#include <filesystem>
 #include <memory>
 
 #include "ll/api/event/EventBus.h"
@@ -15,6 +16,7 @@
 #include "mc/server/commands/MinecraftCommands.h"
 #include "mc/server/commands/ServerCommandOrigin.h"
 #include "mc/world/Minecraft.h"
+
 #include "more_dimensions/api/dimension/CustomDimensionManager.h"
 
 #include "plotcraft/Config.h"
@@ -27,6 +29,7 @@
 #include "plotcraft/utils/Moneys.h"
 #include "remote/Remote.h"
 
+namespace fs = std::filesystem;
 
 namespace my_plugin {
 
@@ -36,18 +39,24 @@ MyPlugin& MyPlugin::getInstance() { return *instance; }
 
 bool MyPlugin::load() {
     auto& logger = getSelf().getLogger();
-    logger.info(R"(    ____   __        __   ______              ____ __ )");
-    logger.info(R"(   / __ \ / /____   / /_ / ____/_____ ____ _ / __// /_)");
-    logger.info(R"(  / /_/ // // __ \ / __// /    / ___// __ `// /_ / __/)");
-    logger.info(R"( / ____// // /_/ // /_ / /___ / /   / /_/ // __// /_  )");
-    logger.info(R"(/_/    /_/ \____/ \__/ \____//_/    \__,_//_/   \__/  )");
-    logger.info(R"(                                                      )");
-    logger.info(R"(            ---- Author: engsr6982 ----               )");
+    logger.info(R"(                                                           )");
+    logger.info(R"(         ____   __        __   ______              ____ __ )");
+    logger.info(R"(        / __ \ / /____   / /_ / ____/_____ ____ _ / __// /_)");
+    logger.info(R"(       / /_/ // // __ \ / __// /    / ___// __ `// /_ / __/)");
+    logger.info(R"(      / ____// // /_/ // /_ / /___ / /   / /_/ // __// /_  )");
+    logger.info(R"(     /_/    /_/ \____/ \__/ \____//_/    \__,_//_/   \__/  )");
+    logger.info(R"(                                                           )");
+    logger.info(R"(                 ---- Author: engsr6982 ----               )");
+    logger.info(R"(                                                           )");
     logger.info("Loading...");
 
 #ifdef DEBUG
     logger.consoleLevel = 5; // 调试模式下输出详细日志
 #endif
+
+    if (!fs::exists(getSelf().getDataDir())) {
+        fs::create_directories(getSelf().getDataDir());
+    }
 
     logger.info("Try loading config、database...");
     plo::config::loadConfig();
