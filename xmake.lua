9abebb70063af9cdad91b4878124681598e1a151
engsr6982 <pwxg286704@163.com>
2025-01-22 13:26:44 +0800

    feat: update dependencies and version

--- xmake.lua
+++ xmake.lua
@@ -1,16 +1,18 @@
 add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
-add_repositories("engsr6982-repo https://github.com/engsr6982/xmake-repo.git")
-
+add_repositories("miracleforest-repo https://github.com/MiracleForest/xmake-repo.git")
 
 -- add_requires("levilamina x.x.x") for a specific version
 -- add_requires("levilamina develop") to use develop version
 -- please note that you should add bdslibrary yourself if using dev version
-add_requires(
-    "levilamina 0.13.5"
-)
-add_requires("more_events 0.2.0")
+if is_config("target_type", "server") then
+    add_requires("levilamina 1.0.0-rc.3", {configs = {target_type = "server"}})
+else
+    add_requires("levilamina 1.0.0-rc.3", {configs = {target_type = "client"}})
+end
+add_requires("levibuildscript")
+add_requires("ilistenattentively 0.2.2")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
@@ -20,10 +22,18 @@ if get_config("overworld") == false then
     add_requires("more-dimensions 0.4.1")
 end 
 
+option("target_type")
+    set_default("server")
+    set_showmenu(true)
+    set_values("server", "client")
+option_end()
+
 option("overworld") -- Overworld
     set_default(false)
 
 target("PlotCraft")
+    add_rules("@levibuildscript/linkrule")
+    add_rules("@levibuildscript/modpacker")
     add_cxflags(
         "/EHa",
         "/utf-8",
@@ -50,15 +60,10 @@ target("PlotCraft")
         "src",
         "include"
     )
-    add_shflags("/DELAYLOAD:bedrock_server.dll") -- To use symbols provided by SymbolProvider.
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
     set_languages("c++20")
     set_symbols("debug")
-
-    if is_mode("debug") then
-        add_defines("DEBUG")
-    end
 
     if get_config("overworld") == false then
         add_packages("more-dimensions")
@@ -71,27 +76,15 @@ target("PlotCraft")
     add_defines("PLUGIN_NAME=\"PlotCraft\"")
     add_defines("PLUGIN_TITLE=\"§6[§aPlotCraft§6]§r \"")
 
-    -- Release 模式下开启最大性能优化
     if is_mode("release") then
         add_cxflags("/O2")
+    elseif is_mode("debug") then
+        add_defines("DEBUG")
     end
 
     after_build(function (target)
-        local plugin_packer = import("scripts.after_build")
-
-        cprint("${bright green}[Build Infomation]: ${reset}Overworld: " .. tostring(get_config("overworld")))
-
-        local tag = os.iorun("git describe --tags --abbrev=0 --always")
-        local major, minor, patch, suffix = tag:match("v(%d+)%.(%d+)%.(%d+)(.*)")
-        if not major then
-            print("Failed to parse version tag, using 0.0.0")
-            major, minor, patch = 0, 0, 0
-        end
-        local plugin_define = {
-            pluginName = target:name(),
-            pluginFile = path.filename(target:targetfile()),
-            pluginVersion = major .. "." .. minor .. "." .. patch,
-        }
-        
-        plugin_packer.pack_plugin(target,plugin_define)
+        local bindir = path.join(os.projectdir(), "bin")
+        local outputdir = path.join(bindir, target:name())
+        local langdir = path.join(os.projectdir(), "assets", "lang")
+        os.cp(langdir, outputdir)
     end)
